5314695e8d8d803c589fbb02bbdfc9ed26eff5d4
Jorge Luiz <devjorgeluiz2000@gmail.com>
2023-06-11 11:36:34 -0400

    estrutura repetição for, while, do while e for of

--- estrutura-de-repetição/app.js
+++ estrutura-de-repetição/app.js
@@ -1,26 +1,25 @@
 const listaArgumentos = process.argv.slice(2);
 
-console.log('----------------execultando FOR-------------------')
-
+console.log('----------------Execultando FOR-------------------')
 for (let controladorFor = 0; controladorFor < listaArgumentos.length; controladorFor++){
     console.log(`Posição ${controladorFor} valor lido = ${listaArgumentos[controladorFor]}`);
 }
 
-console.log('---------------execultando WHILE------------------')
+console.log('---------------Execultando WHILE------------------')
 let controladorWhile = 0;
 while(controladorWhile < listaArgumentos.length){
     console.log(`Posição ${controladorWhile} Valor lido = ${listaArgumentos[controladorWhile]}`);
     controladorWhile++;
 }
 
-console.log('--------------execultando DO WHILE--------------------')
+console.log('--------------Execultando DO WHILE--------------------')
 let controladorDoWhile = 0;
 do{
     console.log(`Posição ${controladorDoWhile} Valor lido = ${listaArgumentos[controladorDoWhile]}`);
     controladorDoWhile++;
 }while(controladorDoWhile < listaArgumentos.length)
 
-console.log('---------------FOR OF------------------')
+console.log('---------------Execultando FOR OF------------------')
 let controladorForOf = 0;
 for(const argumento of listaArgumentos){
     console.log(`Posição ${controladorForOf} Valor lido = ${argumento}`);
